ce781655994750437961efee0a05245375933604
bruno-devlearn <qbruno570@gmail.com>
2025-10-28 09:07:52 -0300

    filter bar + fecth with filter

--- aula004/get-country/app/_utils/(fetchs)/fecth.ts
+++ aula004/get-country/app/_utils/(fetchs)/fecth.ts
@@ -1,13 +1,30 @@
 import axios from "axios";
 import { DataAllProps } from "../(types)/types";
 
-export async function fecthAll(): DataAllProps[] {
+export async function fecther(
+    country?: string,
+    region: string
+): DataAllProps[] {
     try {
-        const res = await axios.get(
-            "https://restcountries.com/v3.1/all?fields=population,region,capital,name"
-        );
-        const data = await res.data;
-        return data;
+        if (country) {
+            const res = await axios.get(
+                `https://restcountries.com/v3.1/name/${country}?fields=population,region,capital,name`
+            );
+
+            return res.data;
+        } else if (region !== "All Regions") {
+            const res = await axios.get(
+                `https://restcountries.com/v3.1/region/${region}?fields=population,region,capital,name`
+            );
+
+            return res.data;
+        } else {
+            const res = await axios.get(
+                "https://restcountries.com/v3.1/all?fields=population,region,capital,name"
+            );
+
+            return res.data;
+        }
     } catch (error) {
         console.error("Erro no fetch:", error);
         return [];
