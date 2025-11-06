733b4ae47b58812bc43926968e9741161f166f3f
bruno-devlearn <qbruno570@gmail.com>
2025-11-06 11:40:36 -0300

    login-page design

--- aula008/mini-twitter/app/page.tsx
+++ aula008/mini-twitter/app/page.tsx
@@ -1,1 +1,28 @@
-export default function Home() {}
+import "./login.css";
+
+export default function Home() {
+    return (
+        <div className="login-container">
+            <div className="login-card">
+                <div className="login-header">
+                    <h1>Bem-vindo</h1>
+                    <span>Entre com seu nome de usuário</span>
+                </div>
+                <form className="login-form">
+                    <div className="input-group">
+                        <label htmlFor="username" className="label-input">
+                            Nome de usuário
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            className="input-field"
+                            placeholder="Digite seu nome..."
+                        />
+                    </div>
+                    <button className="login-button">Entrar</button>
+                </form>
+            </div>
+        </div>
+    );
+}
