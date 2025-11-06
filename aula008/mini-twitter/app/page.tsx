d6ebfd57994e1ee15863f2c62a2f46c6f82bb195
bruno-devlearn <qbruno570@gmail.com>
2025-11-06 12:56:17 -0300

    login page form action

--- aula008/mini-twitter/app/page.tsx
+++ aula008/mini-twitter/app/page.tsx
@@ -1,6 +1,37 @@
+"use client";
+
+import { getUsers, setUsers } from "./_utils/localStorage";
+import { UserProps } from "./_utils/types";
+import { useUser } from "./_utils/store";
 import "./login.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+    const [input, setinput] = useState("");
+
+    const { setCurrentUser } = useUser();
+    const router = useRouter();
+
+    async function handleSubmit(formData: FormData) {
+        const userName = formData.get("username") as string;
+        const users = getUsers();
+
+        const user: UserProps = {
+            userName: userName,
+            posts: [],
+        };
+
+        const exists = users.some((u) => u.userName === userName);
+
+        if (!exists) {
+            setUsers([...users, user]);
+        }
+
+        setCurrentUser(user);
+        router.push("/posts");
+    }
+
     return (
         <div className="login-container">
             <div className="login-card">
@@ -8,7 +39,7 @@ export default function Home() {
                     <h1>Bem-vindo</h1>
                     <span>Entre com seu nome de usuário</span>
                 </div>
-                <form className="login-form">
+                <form className="login-form" action={handleSubmit}>
                     <div className="input-group">
                         <label htmlFor="username" className="label-input">
                             Nome de usuário
@@ -16,11 +47,20 @@ export default function Home() {
                         <input
                             type="text"
                             id="username"
+                            name="username"
                             className="input-field"
                             placeholder="Digite seu nome..."
+                            value={input}
+                            onChange={(e) => setinput(e.target.value)}
                         />
                     </div>
-                    <button className="login-button">Entrar</button>
+                    <button
+                        type="submit"
+                        className="login-button"
+                        disabled={!input.trim()}
+                    >
+                        Entrar
+                    </button>
                 </form>
             </div>
         </div>
